6723b29787c7721bed43c056a549fe299d6be15d
maimai <mailys.andrieu.simplon@gmail.com>
2024-04-25 16:12:38 +0200

    page accueil

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var openProfile = document.getElementById('openProfile')
+var closeProfile = document.getElementById('closeProfile')
+var profil = document.getElementById('profil')
+// openProfile.addEventListener('click', afficherProfil)
+// closeProfile.addEventListener('click', fermerProfil)
+console.log(closeProfile)
+console.log(openProfile)
+console.log(profil)
+
+function afficherProfil(){
+    console.log('ouvrir')
+    closeProfile.style.display= 'block';
+    openProfile.style.display='none';
+    profil.style.display= 'block';
+}
+
+function fermerProfile(){
+    console.log('fermer')
+    console.log(closeProfile)
+    console.log(profil)
+    closeProfile.style.display= 'none';
+    openProfile.style.display='block';
+    profil.style.display= 'none';
+}
